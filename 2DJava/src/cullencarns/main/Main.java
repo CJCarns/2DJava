9093e3b87859590ff424972116822f434482b1a1
Cullen Carns <CJCarns@users.noreply.github.com>
2015-03-28 11:10:42 -0700

    Added Main class, test commit

---
+++ 2DJava/src/cullencarns/main/Main.java
@@ -0,0 +1,12 @@
+package cullencarns.main;
+
+/**
+ *
+ * @author Cullen Carns
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
