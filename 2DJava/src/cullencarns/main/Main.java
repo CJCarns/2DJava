8f2f60201d754cf56a8691a69100a4e7e2bd7fc8
Cullen Carns <CJCarns@users.noreply.github.com>
2015-03-28 11:30:09 -0700

    Command line commit/push test

--- 2DJava/src/cullencarns/main/Main.java
+++ 2DJava/src/cullencarns/main/Main.java
@@ -8,5 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello again");
+        //more changes
     }
 }
